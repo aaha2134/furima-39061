eb55174b4c127ba21a86a48799935f9dfebdbb21
aaha2134 <jacket.mar.jto.6874@gmail.com>
2023-04-04 17:41:22 +0900

    usersテーブルカラム作成

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2023_04_04_072004) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", default: "", null: false
+    t.string "first_name_kanji", default: "", null: false
+    t.string "last_name_kanji", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
