6adbb1fcd676e10d5ac83994701f2c203ee22f00
aaha2134 <jacket.mar.jto.6874@gmail.com>
2023-04-10 20:21:46 +0900

    変更内容の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,41 @@
 class ItemsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new] 
+
+  def index
+  # @items = Item.all
+  # @items = Item.order("created_at DESC")
+  
+  end
+
+  def new
+  @item = Item.new
+  end
+
+  def create
+    
+    @item = Item.new(item_params)
+    
+  if @item.save
+    
+   
+    redirect_to root_path
+    
+  else
+    render:new
+
+  end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product_name, :description, :category_id, :condition_id, :fee_id, :area_id, :deliverytime_id, :price, :user,:user_id,:image).merge(user_id: current_user.id)
+  end
+
+  
+
+
 
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
+  validates :product_name , presence: true
+  validates :description  , presence: true
+  validates :category_id  , presence: true,  numericality: { other_than: 1 }
+  validates :condition_id , presence: true,  numericality: { other_than: 1 }
+  validates :fee_id       , presence: true,  numericality: { other_than: 1 }
+  validates :area_id      , presence: true,  numericality: { other_than: 1 }
+  validates :deliverytime_id , presence: true,  numericality: { other_than: 1 }
+  validates :price        , presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true, message: "は半角数字で入力してください" }
+  validates :image        , presence: true
+
+  belongs_to :user
+  # has_one :order
+  has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :fee
+  belongs_to_active_hash :area
+  belongs_to_active_hash :deliverytime
   
 
   # belongs_to :user
