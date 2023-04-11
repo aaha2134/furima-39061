1bf025482eba93b985e449987923a4c5f492947c
aaha2134 <jacket.mar.jto.6874@gmail.com>
2023-04-11 20:07:36 +0900

    詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  @item = Item.find(params[:id])
+  end
+
+
+
+
+
   private
 
   def item_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+
+  def show
+    user=User.find(params[:id])
+    @items = user.items
+    @nickname = user.nickname
+
+
 end
