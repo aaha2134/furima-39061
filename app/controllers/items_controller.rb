d6a717d78777e45868789ec387581ee06e078315
aaha2134 <jacket.mar.jto.6874@gmail.com>
2023-04-12 17:08:03 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit]
   before_action :set_item, only:[:edit,:show,:update,:destroy]
+  before_action :move_to_index, only:[:edit,:destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -29,10 +30,7 @@ class ItemsController < ApplicationController
 
 
   def edit
-    if current_user != @item.user
-      redirect_to root_path
-  
-    end
+    
 
   end
 
@@ -47,10 +45,10 @@ class ItemsController < ApplicationController
 
 
   def destroy
+  set_item
   @item.destroy
-  redirect_to root_path
-  end
-
+  redirect_to items_path
+end
 
 
   private
@@ -63,5 +61,11 @@ class ItemsController < ApplicationController
   def set_item
   @item = Item.find(params[:id])
   end
+
+  def move_to_index
+    if current_user != @item.user
+      redirect_to root_path
+    end
+end
 
 end
